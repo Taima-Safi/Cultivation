effaf39376ea5cf67f73e59d4fbfa9edda7ad5fc
Taima-safi <safitaima2000@gmail.com>
2024-09-21 12:08:25 +0300

    .

--- Cultivation/Controllers/FertilizerLandController.cs
+++ Cultivation/Controllers/FertilizerLandController.cs
@@ -1,6 +1,8 @@
 ﻿using Cultivation.Dto.FertilizerLand;
 using Cultivation.Repository.FertilizerLand;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.Reflection;
 
 namespace Cultivation.Controllers;
 
@@ -49,5 +51,83 @@ public class FertilizerLandController : ControllerBase
     {
         await fertilizerLandRepo.RemoveAsync(id);
         return Ok();
+    }
+
+    private static (FormFile file, MemoryStream stream) ExportToExcel(ExportType type, string fileName, List<string> filters, List<ExportToExcelDto> dtos)
+    {
+        IWorkbook workbook = new XSSFWorkbook();
+        ISheet sheet = workbook.CreateSheet("Gübre Oygulama");
+
+        // Create cell styles for title.
+        var headerStyle = workbook.CreateCellStyle();
+        IFont font = workbook.CreateFont();
+        font.IsBold = true;
+        headerStyle.WrapText = true;
+        headerStyle.FillForegroundColor = IndexedColors.LightBlue.Index;
+        headerStyle.FillPattern = FillPattern.SolidForeground;
+        headerStyle.Alignment = HorizontalAlignment.Center;
+        headerStyle.VerticalAlignment = VerticalAlignment.Center;
+        headerStyle.SetFont(font);
+
+        // Set the value of a cell
+        switch (type)
+        {
+            case ExportType.LandFertilizers:
+                Type exportDtoType = typeof(ExportToExcelDto);
+                PropertyInfo[] exportDtoProp = exportDtoType.GetProperties();
+                IRow titleRow = sheet.CreateRow(0);
+                int columnNumber = 0;
+                foreach (PropertyInfo property in exportDtoProp)
+                {
+                    //if (filters.Contains(property.Name))
+                    //{
+                    string title = ((DisplayNameAttribute)Attribute.GetCustomAttribute(property, typeof(DisplayNameAttribute))).DisplayName;
+                    titleRow.CreateCell(columnNumber).SetCellValue(title);
+                    titleRow.Cells[columnNumber].CellStyle = headerStyle;
+                    ++columnNumber;
+                    //}
+                }
+                int row = 1;
+                foreach (var dto in dtos)
+                {
+                    int columnInfoNumber = 0;
+                    IRow infoRow = sheet.CreateRow(row);
+                    foreach (PropertyInfo property in exportDtoProp)
+                    {
+                        //if (filters.Contains(property.Name))
+                        //{
+                        var valueOfPatientProp = property.GetValue(dto);
+                        var typeOfPatientProp = property.PropertyType;
+                        if (valueOfPatientProp != null && typeOfPatientProp.IsGenericType)
+                        {
+                            string f = "";
+                            List<string> listValues = Convert.ChangeType(valueOfPatientProp, typeof(List<string>)) as List<string>;
+                            foreach (var item in listValues)
+                            {
+                                f = $"{item}\n";
+                            }
+                            infoRow.CreateCell(columnInfoNumber).SetCellValue(f);
+                        }
+                        else if (valueOfPatientProp != null)
+                            infoRow.CreateCell(columnInfoNumber).SetCellValue(valueOfPatientProp.ToString());
+                        ++columnInfoNumber;
+                        //}
+                    }
+                    row++;
+                }
+                break;
+        }
+        MemoryStream stream = new();
+        workbook.Write(stream);
+        var fileBytes = stream.ToArray();
+        var memoryStream = new MemoryStream(fileBytes);
+        //var fileName = ${fileName+ ".xlsx"};
+        var formFile = new FormFile(memoryStream, 0, memoryStream.Length, null, $"{fileName}.xlsx")
+
+        {
+            Headers = new HeaderDictionary(),
+            ContentType = "application/vnd.ms-excel"
+        };
+        return (formFile, memoryStream);
     }
 }
