6f9838bcc05328abcc37e39d0a3c008cf02f40e0
Taima-safi <safitaima2000@gmail.com>
2024-12-31 21:23:50 +0300

    .

---
+++ Cultivation.Database/Migrations/20241231182326_MixColor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cultivation.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class MixColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Color",
+                table: "FertilizerMix",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "FertilizerMix");
+        }
+    }
+}
