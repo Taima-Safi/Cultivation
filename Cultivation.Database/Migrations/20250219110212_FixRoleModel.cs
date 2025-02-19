43229d2c9bffccd23527cd1ea694278e10cd63b1
Taima-safi <safitaima2000@gmail.com>
2025-02-19 14:49:04 +0300

    files from user-role branch

---
+++ Cultivation.Database/Migrations/20250219110212_FixRoleModel.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cultivation.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRoleModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CuttingLandAccess",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "DepoAccess",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "FullAccess",
+                table: "Role");
+
+            migrationBuilder.DropColumn(
+                name: "OrderAccess",
+                table: "Role");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CuttingLandAccess",
+                table: "Role",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DepoAccess",
+                table: "Role",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "FullAccess",
+                table: "Role",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OrderAccess",
+                table: "Role",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
